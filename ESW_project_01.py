986218a442407d330526db8646cd6b2b0d7f7091
El1  SW <el1as@El1s-MacBook.local>
2025-01-15 15:32:46 -0500

    first commit

---
+++ ESW_project_01.py
@@ -0,0 +1,6 @@
+def main():
+    print ("Hello, I would like to get to know a little about you.")
+    print()
+    print("Please answer a few brief questions")
+if __name__ == "__main__":
+    main()
